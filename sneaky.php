9d47feec9e71019d88a7399b51b474255b0e5378
Chirag Medhora <chiragmedhora@gmail.com>
2013-02-16 21:44:01 -0600

    Update sneaky.php

--- sneaky.php
+++ sneaky.php
@@ -1,20 +1,20 @@
 <?PHP
         unset($log,$log2);
-        if ( /* Small changes with obscenities. */
+        if ( /* Minor edits with obscenities. */
                 (($change['length'] >= -200) and ($change['length'] <= 200))
                 and (($d = $wpi->diff($change['title'],$change['old_revid'],$change['revid'])) or true)
-                and ((($change['title'] == 'User:ClueBot/Sandbox') and print_r($rv)) or true)
-                and (($s = score($obscenelist,$d[0],$log)) or true)
-                and (($s -= score($obscenelist,$d[1],$log2)) or true)
+                and ((($change['title'] == 'User:VinDieselism/Sandbox') and print_r($rv)) or true)
+                and (($s = score($vandalWordsList,$d[0],$log)) or true)
+                and (($s -= score($vandalWordsList,$d[1],$log2)) or true)
                 and (
                         (
-                                ($s < -5) /* There are times when small changes are ok. */
+                                ($s < -5) /* Minor edits with likely false positives. */
                                 and (($rv = $wpapi->revisions($change['title'],2,'older',true,$change['revid'])) or true)
                                 and (!fnmatch('*#REDIRECT*',strtoupper(substr($rv[0]['*'],0,9))))
                                 and (!fnmatch('*SEX*',strtoupper($rv[1]['*'])))
                                 and (!fnmatch('*BDSM*',strtoupper($rv[1]['*'])))
-                                and (score($obscenelist,$change['title']) >= 0)
-                                and (score($obscenelist,$rv[1]['*']) >= 0)
+                                and (score($vandalWordsList,$change['title']) >= 0)
+                                and (score($vandalWordsList,$rv[1]['*']) >= 0)
                                 and (!preg_match('/(^|\s)([a-z]{1,2}(\*+|\-{3,})[a-z]{0,2}|\*{4}|\-{4}|(\<|\()?censored(\>|\))?)(ing?|ed)?(\s|$)/iS',$rv[1]['*']))
                                 and ($heuristic .= '/obscenities')
                                 and ($reason = 'making a minor change with obscenities')
@@ -37,5 +37,5 @@
                                 and ($reason = 'making a minor change adding "!!!"')
                         )
                 )
-        ) { $heuristicret = true; if (isset($log2) and is_array($log2)) foreach ($log2 as $k => $v) $log[$k] -= $v; if (isset($log) and is_array($log)) foreach ($log as $k => $v) if ($v == 0) unset($log[$k]); unset($log2); /* fwrite($irc,'PRIVMSG #wikipedia-BAG/ClueBot :Would revert http://en.wikipedia.org/w/index.php?title='.urlencode($change['namespace'].$change['title']).'&diff=prev'.'&oldid='.urlencode($change['revid'])." .\n"); */ }
+        ) { $heuristicret = true; if (isset($log2) and is_array($log2)) foreach ($log2 as $k => $v) $log[$k] -= $v; if (isset($log) and is_array($log)) foreach ($log as $k => $v) if ($v == 0) unset($log[$k]); unset($log2); /* fwrite($irc,'PRIVMSG #wikipedia-BAG/VinDieselism :Would revert http://en.wikipedia.org/w/index.php?title='.urlencode($change['namespace'].$change['title']).'&diff=prev'.'&oldid='.urlencode($change['revid'])." .\n"); */ }
 ?>
